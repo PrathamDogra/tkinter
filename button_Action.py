de5fa4daab1a0b041128ff3e0f3b68553c705b6f
PrathamDogra <43649218+PrathamDogra@users.noreply.github.com>
2018-10-01 12:18:43 +0530

    changed bg and fg colors.
    changed bg and fg colors of print (button 1).

--- button_Action.py
+++ button_Action.py
@@ -8,7 +8,7 @@ label1=Label(master,text="name")
 label1.pack()
 entrySpace=Entry(master)
 entrySpace.pack()
-button1= Button(master,text="print",command=printName)#command usually perform action everytime button is clicked
+button1= Button(master,text="print",command=printName,fg="red",bg="blue")#command usually perform action everytime button is clicked
 button1.pack() 
 
 master.mainloop()
